6201549234e3335c1c5e38d99c82ad59c5083671
Siddhesh <shirke0personal@gmail.com>
2022-02-13 12:28:25 +0530

    Basic Functionality added: Adding todos to the list.

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,18 +4,64 @@ import DashBoard from "./DashBoard"
 
 export default function Main(props){
     let mode = (props.mode === "dark") ? "" : "light"
+
+    const [todoItem, setTodoItem] = React.useState({
+        isCompleted: false,
+        todoItemText: "",
+        isRemoved: false
+    })
+
+    const [allTodoListItems, setAllTodoListItems] = React.useState([])
+
+    function createTodoItem(event){
+        const {name, value} = event.target
+        setTodoItem((prevTodoItem) => {
+            return {
+                ...prevTodoItem,
+                [name]: value
+            }
+        })
+    }
+
+    function addTodoItemToList(event, todoItem){
+        event.preventDefault()
+        setAllTodoListItems((prevTodoList) => {
+            return [...prevTodoList, todoItem]
+        })
+    }
+    
+    const todoListElements = allTodoListItems.map((list) => {
+        return <TodoItem key={list.todoItemText}
+                         todoItemText={list.todoItemText}
+               />
+    })
+    
     return(
         <main className={mode}>
-        
-            <input type="text" 
-                   placeholder="Create a new todo..."
-                   name="" 
-                // value="" Add it afterwards. Do not keep it empty.
-            />
+            
+            <form onSubmit={(event) =>{
+                addTodoItemToList(event,todoItem)
+            }}
+            >
+                <input type="text" 
+                       placeholder="Create a new todo..."
+                       name="todoItemText" 
+                       value={todoItem.todoItemText}
+                       onChange={createTodoItem}
+                />
+            </form>
 
-            {/* {TodoItem} */}
-
-            <TodoItem /> {/* This is a temporary line added, so that, TodoItem is visible for styling */}
+            {
+                allTodoListItems.length > 0 
+                ? 
+                todoListElements 
+                :
+                <div className="main--empty--list--card">
+                    <p className="main--empty--list--card--text">
+                        Nothing to do!
+                    </p>
+                </div>
+            } 
             
             <DashBoard />
             <p className="main--note">
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,45 +1,15 @@
 import React from "react";
 import cross from "../images/icon-cross.svg"
 
-export default function TodoItem(){
+export default function TodoItem(props){
     return(
         <>
             <div className="todo--item">
                 <div className="todo--item--info">
                     <div className="todo--complete-check"></div>
-                    <p className="todo--item--text">Buy Grocery</p>
-                </div>
-                <img src={cross} className="cross" alt="todo item remover clickable"/>
-            </div>
-
-            <div className="todo--item">
-                <div className="todo--item--info">
-                    <div className="todo--complete-check"></div>
-                    <p className="todo--item--text">Buy Grocery</p>
-                </div>
-                <img src={cross} className="cross" alt="todo item remover clickable"/>
-            </div>
-
-            <div className="todo--item">
-                <div className="todo--item--info">
-                    <div className="todo--complete-check"></div>
-                    <p className="todo--item--text">Buy Grocery</p>
-                </div>
-                <img src={cross} className="cross" alt="todo item remover clickable"/>
-            </div>
-
-            <div className="todo--item">
-                <div className="todo--item--info">
-                    <div className="todo--complete-check"></div>
-                    <p className="todo--item--text">Buy Grocery</p>
-                </div>
-                <img src={cross} className="cross" alt="todo item remover clickable"/>
-            </div>
-
-            <div className="todo--item">
-                <div className="todo--item--info">
-                    <div className="todo--complete-check"></div>
-                    <p className="todo--item--text">Buy Grocery</p>
+                    <p className="todo--item--text">
+                        {props.todoItemText}
+                    </p>
                 </div>
                 <img src={cross} className="cross" alt="todo item remover clickable"/>
             </div>
